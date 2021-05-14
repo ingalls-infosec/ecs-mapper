7bc91a03071b416609a277a67b4fe0a9ea4c37ed
Evans Foster <evans.foster@iinfosec.com>
2021-05-14 15:03:56 -0500

    updates for suricata and adding field list/folders for implementation

---
+++ implementation/convert_kibanaNDJSON_to_csv.py
@@ -0,0 +1,32 @@
+from pandas.io.json import json_normalize
+import json
+import csv
+
+kibana_export_path = '/Users/efoster/Downloads/export(2).ndjson'
+index_name = 'suricata'
+
+csv_file = f"implementation/mapping_csv/{index_name}_fields.csv"
+csv_columns = ['source_field','copy_action','format_action','timestamp_format','destination_field','Notes']
+
+with open(kibana_export_path) as data_file:    
+    d= json.load(data_file)  
+
+fields = json.loads(d['attributes']['fields'])
+
+try:
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for r in fields:
+            if 'esTypes' not in r.keys():
+                r['esTypes'] = ''
+            writer.writerow({
+                'source_field' : r['name'],
+                'copy_action' : '',
+                'format_action' : '',
+                'timestamp_format' : '',
+                'destination_field' : '',
+                'Notes' : f"{r['esTypes']}"
+                })
+except IOError:
+    print("I/O error")
