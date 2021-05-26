452c42629f6badeb5a2f5f15960bfb6c5dd04cc1
Evans Foster <evans.foster@iinfosec.com>
2021-05-26 12:32:36 -0500

    add sentinelone

--- implementation/scripts/convert_kibanaNDJSON_to_csv.py
+++ implementation/scripts/convert_kibanaNDJSON_to_csv.py
@@ -2,8 +2,8 @@ from pandas.io.json import json_normalize
 import json
 import csv
 
-kibana_export_path = '/Users/efoster/Downloads/export(2).ndjson'
-index_name = 'suricata'
+kibana_export_path = '/Users/efoster/Downloads/sentinelone.ndjson'
+index_name = 'sentinelone'
 
 csv_file = f"implementation/mapping_csv/{index_name}_fields.csv"
 csv_columns = ['source_field','copy_action','format_action','timestamp_format','destination_field','Notes']
